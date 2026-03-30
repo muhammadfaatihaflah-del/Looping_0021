c2349eeb0a490739aafdb8aebcc6a3de2571e942
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-30 12:30:39 +0700

    membuat logika or dan memberikan output akhir

--- looping.cpp
+++ looping.cpp
@@ -78,4 +78,11 @@ float nilB,nilM,rerata;
 
     rerata = (nilB + nilM) / 2;
     
+    if ((rerata >= 60) || (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout << "Status kelulusan = " << status 
+         << ", dengan nilai rata-rata = " << rerata << endl;
 }
