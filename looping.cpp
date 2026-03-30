57583becee4027b9e35ec02e98a1b904a78d3d1f
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-30 11:59:08 +0700

    membuat output dan deklarasi bahwa bilangan random akan diambil dari angka 1-10

--- looping.cpp
+++ looping.cpp
@@ -15,4 +15,8 @@ int main()
     cout<<endl;
     //untuk mendapatkan urutan angka acak yang berbeda
     srand(time(0));
+
+    //perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10
 }
